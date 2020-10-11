6e07118c49fa6c7152421e5b46487138ec14561f
RealPolitiX <xrpatrick@gmail.com>
2020-10-11 04:35:11 -0500

    Added PolygonMask class

--- lib/planar2/mask.py
+++ lib/planar2/mask.py
@@ -6,6 +6,7 @@
 
 import numpy as np
 import warnings as wn
+from planar2 import Polygon
 
 try:
     import torch
@@ -13,20 +14,59 @@ except:
     wn.warn('Cannot import pytorch package! Operations using PyTorch Tensors not supported.')
 
 
-def coords2d(center=(0, 0), xlen=64, ylen=64, package='torch'):
+def coords2d(center=(0, 0), xlen=64, ylen=64, package='numpy'):
     """Generate 2D coordinates.
     """
     
     xlen, ylen = int(xlen), int(ylen)
+    center = tuple(map(int, center))
     xaxis = range(-center[1], xlen-center[1])
-    yaxis = range(-center[0], ylen-center[1])
-    ncoords = xlen*ylen
+    yaxis = range(-center[0], ylen-center[0])
+    ncoords = xlen * ylen
     
     if package == 'numpy':
-        out = np.stack(np.meshgrid(yaxis, xaxis), axis=2)
+        coords = np.stack(np.meshgrid(yaxis, xaxis), axis=2)
+        coords = coords.astype('float')
     elif package == 'torch':
         xaxis, yaxis = torch.Tensor(xaxis), torch.Tensor(yaxis)
-        out = torch.stack(torch.meshgrid(yaxis, xaxis), dim=2)
-    out = out.reshape((ncoords, 2))
+        coords = torch.stack(torch.meshgrid(yaxis, xaxis), dim=2)
+    coords = coords.reshape((ncoords, 2))
     
-    return out
+    return coords
+
+
+class PolygonMask(Polygon):
+    """Polygon mask class."""
+    
+    def __init__(self, vertices, **kwargs):
+        super(PolygonMask, self).__init__(vertices, **kwargs)
+        
+    def to_mask(self, xlen=64, ylen=64, package='numpy', fill='constant', fillfunc=1, ret=False):
+        """Convert boundary to mask."""
+        
+        self.xycoords = coords2d(self.centroid, xlen, ylen, package)
+        self.mask = np.asarray(list(map(self.contains_point, self.xycoords)))
+        self.masksize = xlen * ylen
+        
+        if fill == 'constant':
+            self.mask = fillfunc * self.mask
+        elif fill == 'xnan':
+            self.mask[np.where(self.mask == 0)] = np.nan
+        elif fill == 'gaussian':
+            self.mask = _fill_gaussian(self.centroid, self.mask, **kwargs)
+        elif fill == 'edge_decay':
+            self.mask = _fill_edge_decay(self.mask, self.verts, self.mask, **kwargs)
+        elif fill == 'udf': # User-defined function
+            self.mask = fillfunc(self.mask)
+            
+        self.mask = self.mask.reshape((xlen, ylen))
+        if ret:
+            return self.mask
+    
+    @staticmethod
+    def _fill_gaussian(center, mask):
+        pass
+    
+    @staticmethod
+    def _fill_edge_decay(center, verts, mask):
+        pass
